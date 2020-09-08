b31fbd22d4c89116c352d437854864d377b2b784
KannaHashi <khaidirmuktamar234@gmail.com>
2020-09-08 09:29:14 +0800

    berhasil broo!!

---
+++ resources/views/kalkulator/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Kalkulator Murahan</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row justify-content-lg-center">
+            <div class="col col-12-lg">
+                <button class="btn btn-info">=</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
